cc2e4927a1f23354d00ae7f8a92917f5440bff62
Markus Neuhoff <markus.neuhoff@spectrumhealth.org>
2017-02-22 12:42:24 -0500

    adding configuration

--- src/bmi-range/Program.cs
+++ src/bmi-range/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace bmi_range
 {
@@ -12,9 +13,11 @@ namespace bmi_range
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
