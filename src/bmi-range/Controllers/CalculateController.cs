54da3849aa20633b74834cbdbab8c3d039620d23
Markus Neuhoff <markus.neuhoff@spectrumhealth.org>
2017-03-01 14:21:26 -0500

    accepting form input instead of json

--- src/bmi-range/Controllers/CalculateController.cs
+++ src/bmi-range/Controllers/CalculateController.cs
@@ -6,7 +6,7 @@ namespace bmi_range.Controllers
     [Route("api/[controller]")]
     public class CalculateController : Controller
     {
-        public Response Post([FromBody]Request model)
+        public Response Post(Request model)
         {
             if (model != null)
             {
